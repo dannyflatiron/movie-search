d3a195b1e58210b11d7c5f4327d94a842fb0e084
dannyflatiron <dannyflatiron@gmail.com>
2020-12-02 15:53:42 -0500

    create folder for actions

---
+++ src/actions/movies/createMovie.js
@@ -0,0 +1,33 @@
+export const createReview = (newReviewFormData) => {
+  const data = {
+      objectid: newReviewFormData.locationMarker.objectid,
+      content: newReviewFormData.content,
+      user_id: newReviewFormData.userId,
+      location: newReviewFormData.locationMarker.location,
+      name: newReviewFormData.locationMarker.name,
+      ssid: newReviewFormData.locationMarker.ssid,
+      price: newReviewFormData.locationMarker.type
+  }
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/reviews", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(r => r.json())
+      .then(resp => {
+        if (resp.reviewerror) {
+          alert(resp.reviewerror)
+        } else {
+          dispatch(addReview(resp))
+          history.push('/reviews')
+        }
+      }
+      )
+      .catch(console.log)
+
+  }
+}
