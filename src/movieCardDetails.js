24a6eedfa88020b35a54be6b10ff134b87485df9
dannyflatiron <dannyflatiron@gmail.com>
2020-12-04 21:48:52 -0500

    feat: user can view vote count and update vount count

--- src/movieCardDetails.js
+++ src/movieCardDetails.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, {useState} from 'react'
+import { upVote, downVote } from './actions/movies/createMovie.js'
 
 export default function MovieCardDetails({movie}){
 
+  const [upVoteCount, setUpVoteCount] = useState('0')
+  const [downVoteCount, setDownVoteCount] = useState('0')
+
   const handleUpVote = async (event) => {
     event.preventDefault()
-    // await createMovie()
-    // resetVoteCount()
+    await upVote(movie.Title)
+    getAllMovies()
+    
   }
 
   const handleDownVote = async (event) => {
     event.preventDefault()
-    // await createMovie()
-    // resetVoteCount()
+    await downVote(movie.Title)
+    getAllMovies()
+  }
+
+  const getAllMovies = async () => {
+    const movieSearchApi = `http://localhost:3000/movies/`
+
+    fetch(movieSearchApi)
+    .then(response => response.json())
+    .then(result => {
+      let selectedMovie = result.data.map(data => data.attributes).find(m => m.title === movie.Title)
+      if (selectedMovie.upvote === '') {
+        return null 
+      } else {
+        setUpVoteCount(selectedMovie.upvote)
+      } 
+
+      if (selectedMovie.downvote === '' ) {
+        return null 
+      } else {
+        setDownVoteCount(selectedMovie.downvote)
+      } 
+    })
   }
 
 
@@ -24,9 +50,11 @@ export default function MovieCardDetails({movie}){
         <form onSubmit={handleUpVote}>
           <input type="submit" value="Click here to upvote"></input>
         </form>
+        <p>Total upvotes: {upVoteCount}</p>
         <form onSubmit={handleDownVote}>
           <input type="submit" value="Click here to down"></input>
         </form>
+        <p>Total downvotes: {downVoteCount}</p>
       </div>
     )
 }
