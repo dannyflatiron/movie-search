4542af0a00e63b97111f0292f2d825e1293ecc65
dannyflatiron <dannyflatiron@gmail.com>
2020-12-04 21:49:43 -0500

    feat: send movie data to backend to create new object. fix variable naming

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react'
 import MovieCard from './movieCard.js'
+import { createMovie } from './actions/movies/createMovie.js'
 
 export default function SearchMovies() {
 
   const [query, setQuery] = useState('')
-  const [movies, setMovies] = useState([])
+  const [movie, setMovie] = useState([])
   const [hidden, setHidden] = useState(true)
 
-  const searchMovies = async (event) => {
+
+  const searchMovie = async (event) => {
     event.preventDefault()
 
     // const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&language=en-US&query=${query}&page=1&include_adult=false`
@@ -17,20 +19,23 @@ export default function SearchMovies() {
     .then(response => response.json())
     .then(data => {
       setHidden(false)
-      setMovies(data)
+      setMovie(data)
+      createMovie(data)
     })
     setHidden(true)
+    
   }
+
+
 
   return (
 <>
-  <form className="form" onSubmit={searchMovies}>
+  <form className="form" onSubmit={searchMovie}>
     <label className="label" htmlFor="query">Movie Name</label>
     <input className="input" type="text" name="query" placeholder="i.e. Jurassic Park" value={query} onChange={(e) => setQuery(e.target.value)}/>
     <button className="button" type="submit">Search</button>
   </form>
-  {console.log(movies)}
-    {hidden === false ? <MovieCard movie={movies}></MovieCard> : null}
+    {hidden === false ? <MovieCard movie={movie}></MovieCard> : null}
 </>
   )
 }
